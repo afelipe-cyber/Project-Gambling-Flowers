b90389576dcea724099badd5bb46d6bd312a035f
Vieuxfusil <totottef@gmail.com>
2026-02-25 11:37:37 +0100

    ajout de textures pour les items

--- sources/Objets.py
+++ sources/Objets.py
@@ -1,7 +1,8 @@
 class Fleurs:
-    def __init__(self, nom, rareté):
+    def __init__(self, nom, rareté, texture=None):
         self.nom = nom
         self.rareté = rareté
+        self.texture = texture if texture else f"../data/Fleurs/{nom}.png"
 
     def affichage_fl(self):
         # Retourne une représentation unicode simple de la fleur
@@ -53,9 +54,10 @@ fleurs = {
 }
 
 class Graines:
-    def __init__(self, nom, rareté):
+    def __init__(self, nom, rareté, texture=None):
         self.nom = nom
         self.rareté = rareté
+        self.texture = texture if texture else f"../data/Graines/{nom}.png"
 
     def affichage_gr(self):
         # Retourne une représentation unicode simple de la graine
@@ -80,36 +82,36 @@ class Graines:
         return f"{self.nom} de rareté {self.rareté}"
     
 graines = {
-    "Cécilia": Graines("Graine de Cécilia", 0),
-    "Dendrobium sanglant": Graines("Graine de Dendrobium sanglant", 0),
-    "Chrysanthème": Graines("Graine de Chrysanthème", 0),
-    "Fleur de Qingxin": Graines("Graine de Fleur de Qingxin", 0),
-    "Fleur de soie": Graines("Graine de Fleur de soie", 0),
-    "Fleur funéraire": Graines("Graine de Fleur funéraire", 0),
-    "Fleur sucrante": Graines("Graine de Fleur sucrante", 0),
-    "Gueule-de-loup": Graines("Graine de Gueule-de-loup", 0),
-    "Herbe à lampe": Graines("Graine de Herbe à lampe", 0),
-    "Herbe à sanglots": Graines("Graine de Herbe à sanglots", 0),
-    "Inteyvat": Graines("Graine de Inteyvat", 0),
-    "Kalpalotus": Graines("Graine de Kalpalotus", 0),
-    "Lotus pluvieux": Graines("Graine de Lotus pluvieux", 0),
-    "Lys calla": Graines("Graine de Lys calla", 0),
-    "Lys lacmineux": Graines("Graine de Lys lacmineux", 0),
-    "Lys verni": Graines("Graine de Lys verni", 0),
-    "Marcotte": Graines("Graine de Marcotte", 0),
-    "Muget bleu": Graines("Graine de Muget bleu", 0),
-    "Padisachidée": Graines("Graine de Padisachidée", 0),
-    "Rose arc-en-ciel": Graines("Graine de Rose arc-en-ciel", 0),
+    "Cécilia": Graines("Graines de Cécilia", 0),
+    "Dendrobium sanglant": Graines("Graines de Dendrobium sanglant", 0),
+    "Chrysanthème": Graines("Graines de Chrysanthèmes", 0),
+    "Fleur de Qingxin": Graines("Graines de Fleur de Qingxin", 0),
+    "Fleur de soie": Graines("Graines de Fleur de soie", 0),
+    "Fleur funéraire": Graines("Graines de Fleur funéraire", 0),
+    "Fleur sucrante": Graines("Graines de Fleur sucrante", 0),
+    "Gueule-de-loup": Graines("Graines de Gueule de loup", 0),
+    "Herbe à lampe": Graines("Graines d'Herbe à lampe", 0),
+    "Herbe à sanglots": Graines("Graines d'Herbe à sanglots", 0),
+    "Inteyvat": Graines("Graines de Inteyvat", 0),
+    "Kalpalotus": Graines("Graines de Kalpalotus", 0),
+    "Lotus pluvieux": Graines("Graines de Lotus pluvieux", 0),
+    "Lys calla": Graines("Graines de Lys calla", 0),
+    "Lys lacmineux": Graines("Graines de Lys lacmineux", 0),
+    "Lys verni": Graines("Graines de Lys verni", 0),
+    "Marcotte": Graines("Graines de Marcotte", 0),
+    "Muget bleu": Graines("Graines de Muget bleu", 0),
+    "Padisachidée": Graines("Graines de Padisachidée", 0),
+    "Rose arc-en-ciel": Graines("Graines de Rose arc-en-ciel", 0),
     "Rose sumérienne": Graines("Graine de Rose sumérienne", 0),
-    "Viparyas": Graines("Graine de Viparyas", 0),
+    "Viparyas": Graines("Graines de Viparyas", 0),
 
 }
 
-# # Test
-# print("Fleurs disponibles :")
-# for nom, fleur in fleurs.items():
-#     print(fleur.affichage_fl())
+# Test
+print("Fleurs disponibles :")
+for nom, fleur in fleurs.items():
+    print(fleur.affichage_fl())
 
-# print("\nGraines disponibles :")
-# for nom, graine in Graines.items():
-#     print(graine.affichage_gr())
+print("\nGraines disponibles :")
+for nom, graine in Graines.items():
+    print(graine.affichage_gr())
